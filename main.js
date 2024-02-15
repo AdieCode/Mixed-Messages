ea91fc892d9953ec8dd0f8e8b89fb08d20bd1009
AdieCode <Adiecode4@gmail.com>
2024-02-16 01:19:54 +0200

    Added the data that will be used to construct the jokes.

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-const Name = ['Jimmy', 'Jhon', 'Kelly', 'Greg', 'Debbie']
+const name = [{name:'Jimmy',
+               pronoun: 'he'
+            },
+               {name:'Jhon',
+               pronoun: 'he'
+            },
+               {name:'Kelly',
+               pronoun: 'she'
+            },
+               {name:'Greg',
+               pronoun: 'he'
+            },
+               {name:'Debbie',
+               pronoun: 'she'
+            }
+            ]
+
+
+const JokeWords = [{ lineUpWord : '"Class dismissed"',
+                    punchLine: 'finished with the lesson'
+                },
+                    {lineUpWord : '"Case closed"',
+                    punchLine: 'He solved the mystery'
+                },
+                    {lineUpWord : '"Class dismissed"',
+                    punchLine: 'finished with the lesson'
+                },
+                    {lineUpWord : '"Game on"',
+                    punchLine: 'ready to compete'
+                },
+                
+]
